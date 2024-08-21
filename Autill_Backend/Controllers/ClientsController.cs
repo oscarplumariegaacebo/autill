a8219fad40471299aab2e489bd7f28544f636cd6
Oscar Plumariega <oscarplumariegacebo@gmail.com>
2024-08-22 00:57:32 +0200

    ClientsController

---
+++ Autill_Backend/Controllers/ClientsController.cs
@@ -0,0 +1,88 @@
+﻿using Autill.Data;
+using Autill.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Autill.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly ClientContext _clientContext;
+
+        public ClientsController(ClientContext clientContext)
+        {
+            _clientContext = clientContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClients()
+        {
+            return await _clientContext.Clients.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Client>> GetClient(int id)
+        {
+            var client = await _clientContext.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+            return client;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClient(int id, Client client)
+        {
+            if (id != client.Id)
+            {
+                return BadRequest();
+            }
+            _clientContext.Entry(client).State = EntityState.Modified;
+            try
+            {
+                await _clientContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Client>> PostClient(Client client)
+        {
+            _clientContext.Clients.Add(client);
+            await _clientContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetClient), new { id = client.Id }, client);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClient(int id)
+        {
+            var client = await _clientContext.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+            _clientContext.Clients.Remove(client);
+            await _clientContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private bool ClientExists(int id)
+        {
+            return _clientContext.Clients.Any(e => e.Id == id);
+        }
+    }
+}
--- Autill_Backend/obj/Debug/net8.0/Autill.AssemblyInfo.cs
+++ Autill_Backend/obj/Debug/net8.0/Autill.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Autill")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af47c89d04356ae27974264bdf84f4d331663550")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65540acf7ac6ab587f357eb5ac84b55e3e2abe31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Autill")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Autill")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
