06009f29e25464cdcbcf488e2c79f1552e07f3c0
Oscar Plumariega <oscarplumariegacebo@gmail.com>
2024-10-13 22:27:47 +0200

    bug adding items fixed

--- Autill/src/app/shared/components/budget-details/budget-details.component.ts
+++ Autill/src/app/shared/components/budget-details/budget-details.component.ts
@@ -5,8 +5,8 @@ import { ApiService } from '../../../core/services/api.service';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {AsyncPipe} from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { AsyncPipe } from '@angular/common';
 import { map, Observable, startWith } from 'rxjs';
 import { Item } from '../../../core/models/Item';
 import { ItemService } from '../../../core/services/item.service';
@@ -15,25 +15,24 @@ import { ItemService } from '../../../core/services/item.service';
 @Component({
   selector: 'app-budget-details',
   standalone: true,
-  imports: [FormsModule,MatFormFieldModule,MatInputModule,MatAutocompleteModule,ReactiveFormsModule,AsyncPipe],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatAutocompleteModule, ReactiveFormsModule, AsyncPipe],
   templateUrl: './budget-details.component.html',
   styleUrl: './budget-details.component.css'
 })
 export class BudgetDetailsComponent {
-  items = [{id: 0, name: '', units: 0, price: 0, totalConcept: 0}];
+  items = [{ id: 0, name: '', units: 0, price: 0, totalConcept: 0 }];
   data = [];
-  dbItems:any = [];
+  dbItems: any = [];
   apiService = inject(ApiService);
   itemService = inject(ItemService);
   Item = new FormControl<ItemInit | string>('');
   filteredItems!: Observable<Item[]>;
-  lastOptionIdSelected:number = 0;
+  lastOptionIdSelected: number = 0;
   lasItemAdded: any = {};
   detailsForm!: FormGroup;
   nameDefault: string = '';
-  itemsData = [];
 
-  initializeForm(){
+  initializeForm() {
     this.detailsForm = new FormGroup({
       Item0: new FormControl(),
       PriceTD0: new FormControl(),
@@ -41,18 +40,16 @@ export class BudgetDetailsComponent {
     })
   }
 
-  constructor(public dialogRef: MatDialogRef<BudgetDetailsComponent>){
+  constructor(public dialogRef: MatDialogRef<BudgetDetailsComponent>) {
     this.initializeForm();
   }
 
   ngOnInit() {
-    if(this.data.length > 0){
+    if (this.data.length > 0) {
       this.items = this.data;
-      this.itemsData = this.data;
-      console.log(this.itemsData);
     }
 
-    this.itemService.getItems().subscribe((data:any) => {
+    this.itemService.getItems().subscribe((data: any) => {
       this.dbItems = data;
 
       this.filteredItems = this.detailsForm.controls['Item0'].valueChanges.pipe(
@@ -65,7 +62,7 @@ export class BudgetDetailsComponent {
     })
 
     for (let i = 0; i < this.items.length; i++) {
-      if(i>0){
+      if (i > 0) {
         this.detailsForm.addControl(`Item${i}`, new FormControl());
       }
     }
@@ -80,49 +77,28 @@ export class BudgetDetailsComponent {
   }
 
   onClose(): void {
-    if(this.items[this.items.length - 1].totalConcept == 0){
+    if (this.items[this.items.length - 1].totalConcept == 0) {
       this.items.pop();
     }
-    this.dialogRef.close({data: this.items});
+    this.dialogRef.close({ data: this.items });
   }
 
-  addItem(id:number,from:string){
-    if(id+1 === this.items.length){
-      this.detailsForm.addControl(`Item${id+1}`, new FormControl());
-      this.detailsForm.addControl(`PriceTD${id+1}`, new FormControl());
-      this.detailsForm.addControl(`Units${id+1}`, new FormControl());
+  addItem(id: number, type:string) {
+    if(type === 'form'){
+      this.newFormControls(id);
 
-      this.items.push({id: id+1, name: '', units: 0, price: 0, totalConcept: 0});
+      this.items.push({ id: id, name: '', units: 0, price: 0, totalConcept: 0 });
     }else{
-      let unitsInput = document.getElementById('units'+id) as HTMLInputElement;
-      let name = this.detailsForm.controls['Item'+id].value;
-      let price = document.getElementById('priceTD'+id) as HTMLInputElement;
+      let units = parseFloat(this.detailsForm.controls['Units' + id].value);
   
-      let units = parseFloat(unitsInput.value);
-  
+      this.items[id].name = this.lasItemAdded.name;
       this.items[id].units = units;
-      if(from === 'newItem'){
-        this.detailsForm.addControl(`Item${id+1}`, new FormControl());
-        this.detailsForm.addControl(`PriceTD${id+1}`, new FormControl());
-        this.detailsForm.addControl(`Units${id+1}`, new FormControl());
+      this.items[id].price = parseFloat(this.lasItemAdded.price);
+      this.items[id].totalConcept = units * parseFloat(this.lasItemAdded.price);
+  
+      this.items.push({ id: id + 1, name: '', units: 0, price: 0, totalConcept: 0 });
 
-        this.items[id].name = this.lasItemAdded.name;
-        this.items[id].price = parseFloat(this.lasItemAdded.price);
-        this.items[id].totalConcept = parseFloat(unitsInput.value) * parseFloat(this.lasItemAdded.price);
-
-        this.items.push({id: id+1, name: '', units: 0, price: 0, totalConcept: 0});
-      }else{
-        console.log(this.items[id]);
-        this.items[id].name = name;
-        this.items[id].price = parseFloat(price.value);
-        this.items[id].totalConcept = units * this.items[id].price;
-
-        this.detailsForm.get(`Item${id+1}`)!.setValue(this.items[id].name);
-        this.detailsForm.get(`Units${id+1}`)!.setValue(this.items[id].units);
-        this.detailsForm.get(`PriceTD${id+1}`)!.setValue(this.items[id].price);
-      }
-
-      this.filteredItems = this.detailsForm.controls[`Item${id+1}`].valueChanges.pipe(
+      this.filteredItems = this.detailsForm.controls[`Item${id}`].valueChanges.pipe(
         startWith(''),
         map(value => {
           const item = value;
@@ -132,7 +108,13 @@ export class BudgetDetailsComponent {
     }
   }
 
-  changeSelection(id: number, name: string, event: any){
+  newFormControls(id: number) {
+    this.detailsForm.addControl(`Item${id}`, new FormControl());
+    this.detailsForm.addControl(`PriceTD${id}`, new FormControl());
+    this.detailsForm.addControl(`Units${id}`, new FormControl());
+  }
+
+  changeSelection(id: number, name: string, event: any) {
     if (event.isUserInput) {
       this.lastOptionIdSelected = id;
       this._filter(name);
@@ -142,27 +124,26 @@ export class BudgetDetailsComponent {
   private _filter(value: any): any[] {
     const filterValue = typeof value === 'string' ? value.toLowerCase() : value.Item.toLowerCase();
 
-    let itemSelected = this.dbItems.find((item:any) => item.name === value)!;
+    let itemSelected = this.dbItems.find((item: any) => item.name === value)!;
 
-    if(itemSelected !== undefined) {
+    if (itemSelected !== undefined) {
       let priceElement = document.getElementById(`priceTD${this.lastOptionIdSelected}`)! as HTMLInputElement;
       priceElement.value = itemSelected.price;
     }
 
     this.lasItemAdded = itemSelected;
 
-    return this.dbItems.filter((option:any) => option.name.toLowerCase().includes(filterValue));
+    return this.dbItems.filter((option: any) => option.name.toLowerCase().includes(filterValue));
   }
 
-  addItems(){
-    this.addItem(this.items.length-1, 'newItem');
-    console.log(this.detailsForm);
+  addItems() {
+    this.addItem(this.items.length - 1, 'new');
     //this.items.pop();
 
     this.onClose();
   }
 
-  unitsChange(idItem: number, event: any){
+  unitsChange(idItem: number, event: any) {
     let refreshUnits = (event.target as HTMLInputElement).value
 
     this.items[idItem].units = parseFloat(refreshUnits);
